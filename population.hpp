63e8d23c89393005de7f61914792004f6a361a27
titofra <120715525+titofra@users.noreply.github.com>
2022-12-26 21:06:23 +0000

    Update population.hpp

--- population.hpp
+++ population.hpp
@@ -39,7 +39,7 @@ class Population{
 		void runNetworkAuto(float processFn(float procInputs[], float procOutputs[], struct args* args), struct args* args, void initArgsInputs(float inputs[], struct args* args), float activationFn(float input), int maxIterationThresh, float fitnessOnMaxIteration = 0.0f);
 		void runNetworkAuto(float processFn(float procInputs[], float procOutputs[], struct args* args), struct args* args, void initArgsInputs(float inputs[], struct args* args), float activationFn(float input), int maxIterationThresh, float fitnessOnMaxIteration, int genomeId);
 		void setFitness(float fitness, int genomeId);
-		void speciate(int target = 5, int targetThresh = 1, float stepThresh = 0.5f, float a = 1.0f, float b = 1.0f, float c = 0.4f);
+		void speciate(int target = 5, int targetThresh = 0, float stepThresh = 0.5f, float a = 1.0f, float b = 1.0f, float c = 0.4f);
 		void crossover(bool elitism = false);	// git error#3
 		void mutate(float mutateWeightThresh = 0.8f, float mutateWeightFullChangeThresh = 0.1f, float mutateWeightFactor = 1.2f, float addConnectionThresh = 0.05f, int maxIterationsFindConnectionThresh = 20, float reactivateConnectionThresh = 0.25f, float addNodeThresh = 0.03f, int maxIterationsFindNodeThresh = 20);
 		void drawNetwork(int genomeId, sf::Vector2u windowSize = {1300, 800}, float dotsRadius = 6.5f);
